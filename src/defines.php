2f83128f52c867a188cd794223ccfdcef3abfcd1
Christoph <info@cmeier-software.com>
2023-12-06 14:33:52 +0800

    correct paths

--- src/defines.php
+++ src/defines.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
 if (!defined('ABS_PATH')) {
-    define('ABS_PATH', realpath(__DIR__ . '/..'));
+    define('ABS_PATH', realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR );
 }
 
 if (!defined('CONFIG_DIR')) {
-    define('CONFIG_DIR', ABS_PATH . '/config');
+    define('CONFIG_DIR', ABS_PATH . 'config'. DIRECTORY_SEPARATOR );
 }
 
 require_once ABS_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
