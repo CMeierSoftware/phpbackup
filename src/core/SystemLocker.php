1367523eea665813e109f18ed013b7b344e64143
Christoph <info@cmeier-software.com>
2023-12-06 16:46:12 +0800

    Add systemLocker

---
+++ src/core/SystemLocker.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+/**
+* This script locks a directory.
+* It is not locked for write or read.
+* It only create a file, which indicates that there is something running, which do not want to disturbed.
+* 
+*   try{
+*         SystemLocker::lock();
+*       // Do some thing
+*    }
+*    finally{
+*        SystemLocker::unlock();
+*    }
+*
+* @author: Christoph Meier (CMeier Software)
+* @version 1.1.1
+* @date: 25.02.2023
+*/
+namespace CMS\PhpBackup\Core;
+
+use Exception;
+
+define('LOCK_TS', date('Y.m.d-H:i:s', time()));
+
+final class SystemLockedException extends Exception {}
+
+abstract class SystemLocker
+{
+    public const DEFAULT_LOCK_FILE = '.lock_system';
+
+    /**
+     * Function tries to lock the system.
+     * Throws an Exception if can not lock the system.
+     */
+    public static function lock(string $system_path): void
+    {
+        if (self::isLocked($system_path)) {
+            throw new SystemLockedException('System-Locker: System is locked since ' . self::readLockFile($system_path) . ' UTC.');
+        }
+
+        $result = file_put_contents(self::getLockFilePath($system_path), LOCK_TS);
+
+        if ($result === false and !self::isLocked($system_path)) {
+            throw new Exception('System-Locker: Can not lock system.');
+        }
+    }
+
+    /*
+     * Function checks if the system is locked.
+     * @return bool is system locked
+     */
+    public static function isLocked(string $system_path): bool
+    {
+        return file_exists(self::getLockFilePath($system_path));
+    }
+
+    /*
+     * Function tries to unlock the system.
+     * It only unlocks it, if this instance locked it.
+     */
+    public static function unlock(string $system_path): void
+    {
+        if (self::readLockFile($system_path) == LOCK_TS) {
+            unlink(self::getLockFilePath($system_path));
+        }
+    }
+
+    /*
+     * Function reads content of the lock file
+     * @return The entire file in a string, FALSE on failure
+     */
+    public static function readLockFile(string $system_path): string
+    {
+        if (self::isLocked($system_path)) {
+            return file_get_contents(self::getLockFilePath($system_path));
+        }
+
+        return '';
+    }
+
+    private static function getLockFilePath(string $system_path): string
+    {
+        return $system_path . DIRECTORY_SEPARATOR . self::DEFAULT_LOCK_FILE;
+    }
+}
+
---
+++ tests/core/SystemLockerTest.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types=1);
+
+namespace CMS\PhpBackup\Tests;
+
+use PHPUnit\Framework\TestCase;
+use CMS\PhpBackup\Core\SystemLocker;
+use CMS\PhpBackup\Core\SystemLockedException;
+
+class SystemLockerTest extends TestCase
+{
+    private string $system_path = __DIR__ . "/../work";
+
+    public function setUp(): void
+    {
+        // Clean up before each test
+        if (file_exists($this->system_path . '/.lock_system')) {
+            unlink($this->system_path . '/.lock_system');
+        }
+    }
+
+    /**
+     * @covers lock
+     * @covers unlock
+     * @uses SystemLocker::isLocked
+     * @uses SystemLocker::readLockFile
+     */
+    public function testLockSystem()
+    {
+        // Lock the system
+        SystemLocker::lock($this->system_path);
+
+        // Assert that the system is locked
+        $this->assertTrue(SystemLocker::isLocked($this->system_path));
+
+        // Assert that reading the lock file returns a timestamp
+        $lockTimestamp = SystemLocker::readLockFile($this->system_path);
+        $this->assertNotEmpty($lockTimestamp);
+
+        // Clean up: Unlock the system
+        SystemLocker::unlock($this->system_path);
+    }
+
+    /**
+     * @covers lock
+     * @uses unlock
+     */
+    public function testLockedExceptionOnDoubleLock()
+    {
+        // Lock the system
+        SystemLocker::lock($this->system_path);
+
+        // Attempt to lock the system again, expect SystemLockedException
+        $this->expectException(SystemLockedException::class);
+        SystemLocker::lock($this->system_path);
+
+        // Clean up: Unlock the system
+        SystemLocker::unlock($this->system_path);
+    }
+
+    /**
+     * @covers lock
+     * @covers unlock
+     */
+    public function testUnlockDoesNotAffectOtherLocks()
+    {
+        $otherSystemPath =  __DIR__ . "/../work2";
+        mkdir($otherSystemPath);
+        try {
+            // Lock the system
+            SystemLocker::lock($this->system_path);
+    
+            // Create another instance with a different path
+            SystemLocker::lock($otherSystemPath);
+            
+            // Unlock the system with the other path
+            SystemLocker::unlock($otherSystemPath);
+    
+            // Assert that the original system is still locked
+            $this->assertTrue(SystemLocker::isLocked($this->system_path));
+    
+            // Clean up: Unlock the original system
+            SystemLocker::unlock($this->system_path);
+        } finally{
+            rmdir($otherSystemPath);
+        }
+        
+    }
+
+
+    /**
+     * @covers readLockFile
+     */
+    public function testReadLockFileWhenLocked()
+    {
+        // Lock the system
+        SystemLocker::lock($this->system_path);
+
+        // Read the lock file
+        $lockTimestamp = SystemLocker::readLockFile($this->system_path);
+
+        // Assert that the lock timestamp is not empty
+        $this->assertNotEmpty($lockTimestamp);
+
+        // Clean up: Unlock the system
+        SystemLocker::unlock($this->system_path);
+    }
+
+    /**
+     * @covers readLockFile
+     */
+    public function testReadLockFileWhenUnlocked()
+    {
+        // Read the lock file when the system is not locked
+        $lockTimestamp = SystemLocker::readLockFile($this->system_path);
+
+        // Assert that the lock timestamp is empty
+        $this->assertEmpty($lockTimestamp);
+    }
+}
