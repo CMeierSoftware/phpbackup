465f8f4f3cd8b13b0ffdc4885990013625c945b8
Christoph <info@cmeier-software.com>
2023-12-06 15:21:12 +0800

    Add LogLevels

---
+++ src/core/LogLevel.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+/**
+ * This script is a simple logger that writes messages into a file.
+ *
+ * Usage:
+ *
+ * try {
+ *     FileLogger::getInstance()->info('Info Message');
+ *     // Do some thing
+ * } catch(Exception $e) {
+ *     FileLogger::getInstance()->error($e->getMessage());
+ * }
+ *
+ * @package FileLogger
+ * @version 1.0.0
+ * @author Christoph Meier
+ * @date 19.12.2021
+ */
+
+namespace CMS\PhpBackup\Core;
+
+use Exception;
+
+abstract class LogLevel
+{
+    public const OFF = 0;
+    public const ERROR = 1;
+    public const WARNING = 2;
+    public const INFO = 3;
+
+    /**
+     * Converts a log level integer value to its string representation.
+     *
+     * @param int $level The log level integer value to convert.
+     * @return string The string representation of the log level.
+     * @throws Exception If the given log level integer value is invalid.
+     */
+    public static function toString(int $level): string
+    {
+        switch ($level) {
+            case LogLevel::OFF:
+                return 'OFF';
+            case LogLevel::ERROR:
+                return 'ERROR';
+            case LogLevel::WARNING:
+                return 'WARNING';
+            case LogLevel::INFO:
+                return 'INFO';
+            default:
+                throw new Exception("Invalid log level: $level");
+        }
+    }
+}
---
+++ tests/core/LogLevelTest.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+use CMS\PhpBackup\Core\LogLevel;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers \LogLevel
+ */
+class LogLevelTest extends TestCase
+{
+    /**
+     * @codeCoverageIgnore
+     */
+    public function testLogLevelsConstants()
+    {
+        $this->assertEquals(0, LogLevel::OFF);
+        $this->assertEquals(1, LogLevel::ERROR);
+        $this->assertEquals(2, LogLevel::WARNING);
+        $this->assertEquals(3, LogLevel::INFO);
+    }
+
+    /**
+     * @covers toString
+     */
+    public function testToStringForOff()
+    {
+        $result = LogLevel::toString(LogLevel::OFF);
+        $this->assertEquals('OFF', $result);
+    }
+
+    /**
+     * @covers toString
+     */
+    public function testToStringForError()
+    {
+        $result = LogLevel::toString(LogLevel::ERROR);
+        $this->assertEquals('ERROR', $result);
+    }
+
+    /**
+     * @covers toString
+     */
+    public function testToStringForWarning()
+    {
+        $result = LogLevel::toString(LogLevel::WARNING);
+        $this->assertEquals('WARNING', $result);
+    }
+
+    /**
+     * @covers toString
+     */
+    public function testToStringForInfo()
+    {
+        $result = LogLevel::toString(LogLevel::INFO);
+        $this->assertEquals('INFO', $result);
+    }
+
+    /**
+     * @covers toString
+     */
+    public function testToStringThrowsExceptionForInvalidLogLevel()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Invalid log level: 42");
+
+        LogLevel::toString(42);
+    }
+}
