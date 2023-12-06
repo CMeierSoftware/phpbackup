ca5139ecbf6d2c70af8cf494898c3f1efc3ec8b1
Christoph <info@cmeier-software.com>
2023-12-06 14:40:53 +0800

    Add class to handle settings

---
+++ src/core/ProjectSettings.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Cms\Phpbackup\core;
+
+use Laminas\Config\Factory as LaminasConfigFactory;
+use Laminas\Config\Config;
+
+/**
+ * Class ProjectSettings
+ *
+ * A singleton class for managing project settings using laminas-config.
+ *
+ * @package Cms\Phpbackup\core
+ */
+class ProjectSettings
+{
+    private const CONFIG_FILE = CONFIG_DIR . DIRECTORY_SEPARATOR . "config.json";
+    private static ProjectSettings $instance;
+    private static Config $config;
+
+    /**
+     * Get an instance of the ProjectSettings class.
+     *
+     * @return ProjectSettings
+     */
+    public static function getInstance(): ProjectSettings
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+            self::$config = new Config(LaminasConfigFactory::fromFile(self::CONFIG_FILE, true));
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Save the configuration to the file.
+     *
+     */
+    public function save(): void
+    {
+        if (self::$config) {
+            LaminasConfigFactory::toFile(self::CONFIG_FILE, self::$config);
+        }
+    }
+}
---
+++ tests/core/ProjectSettingsTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types= 1);
+
+namespace Cms\Phpbackup\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Cms\Phpbackup\core\ProjectSettings;
+
+class ProjectSettingsTest extends TestCase
+{        
+    /**
+     * @covers \getInstance
+     */
+    public function testSingletonInstance()
+    {
+        $instance1 = ProjectSettings::getInstance();
+        $instance2 = ProjectSettings::getInstance();
+
+        $this->assertInstanceOf(ProjectSettings::class, $instance1);
+        $this->assertInstanceOf(ProjectSettings::class, $instance2);
+        $this->assertEquals($instance1, $instance2);
+    }
+}
