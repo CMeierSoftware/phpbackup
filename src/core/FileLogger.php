e9906065d046a384e96480434b78566ff8f68454
Christoph <info@cmeier-software.com>
2023-12-06 16:08:12 +0800

    Add FileLogger

---
+++ src/core/FileLogger.php
@@ -0,0 +1,193 @@
+<?php declare(strict_types=1);
+
+/**
+ * This script is a simple logger that writes messages into a file.
+ *
+ * Usage:
+ *
+ * try {
+ *     FileLogger::getInstance()->info('Info Message');
+ *     // Do some thing
+ * } catch(Exception $e) {
+ *     FileLogger::getInstance()->error($e->getMessage());
+ * }
+ *
+ * @package FileLogger
+ * @version 1.0.0
+ * @author Christoph Meier
+ * @date 19.12.2021
+ */
+
+namespace CMS\PhpBackup\Core;
+
+use CMS\PhpBackup\Core\LogLevel;
+
+class FileLogger
+{	
+	public const DEFAULT_LOG_FILE = './logs.txt';
+
+	protected static ?FileLogger $instance = null;
+	private string $log_file;
+	private int $log_level;
+	private bool $echo_logs;
+	
+	 /**
+     * Constructs the logger instance.
+     *
+     * @param string $log_file The path to the log file.
+     * @param int $log_level The log level to use.
+     * @param bool $echo_logs Whether to echo log messages in HTML format.
+     */
+    protected function __construct(string $log_file, int $log_level, bool $echo_logs)
+    {
+        $this->log_file = $log_file;
+        $this->log_level = $log_level;
+        $this->echo_logs = $echo_logs;
+    }
+
+    /**
+     * Clones the logger instance (disallowed).
+     */
+    protected function __clone()
+    {
+    }
+
+    /**
+     * Gets the singleton instance of the logger.
+     *
+     * @param string $log_file The path to the log file (optional, default: `logs.txt`).
+     * @param int $log_level The log level to use (optional, default: `LogLevel::WARNING`).
+     * @param bool $echo_logs Whether to echo log messages in HTML format (optional, default: `false`).
+     * @return FileLogger The singleton instance of the logger.
+     */
+    public static function getInstance(string $log_file = self::DEFAULT_LOG_FILE, int $log_level = LogLevel::WARNING, bool $echo_logs = false): FileLogger
+    {
+        if (null === self::$instance) {
+            self::$instance = new self($log_file, $log_level, $echo_logs);
+        }
+        return self::$instance;
+    }
+
+	/*
+	* Function sets a new path to a log file. This new file will be created if it still not exists.
+	*
+	* param
+	*	$log_file: path to the new log file. WARNING: The directory must already exists.
+	* @return 
+	*	void
+	*/
+	public function SetLogFile(string $log_file)
+	{
+		$this->log_file = $log_file;
+		FileLogger::GetInstance()->Info("set log file to $log_file");
+	}
+	
+	/*
+	* activates 'echo logs'.
+	* IF you activate 'echo logs', the logger will echo the logs in the file AND will print it as HTML.
+	* @return 
+	*	void
+	*/
+	public function ActivateEchoLogs()
+	{
+		$this->echo_logs = true;
+	}
+		
+	/*
+	* Deactivates 'echo logs'.
+	* @return 
+	*	void
+	*/
+	public function DeactivateEchoLogs()
+	{
+		$this->echo_logs = false;
+	}
+		
+	/*
+	* Set the Log level
+	* Type must be from 'LogLevel'
+	*/
+	public function SetLogLevel(int $log_level): void
+	{
+        self::GetInstance()->Info('set log level to ' . LogLevel::toString($log_level));
+		$this->log_level = $log_level;
+	}
+	
+	/*
+	* Writes a message of the level 'Error'
+	*/
+	public function Error(string $message): void
+	{
+		$this->WriteEntry(LogLevel::ERROR, $message);
+	}
+	
+	/*
+	* Writes a message of the level 'Warning'
+	* @return 
+	*	void
+	*/
+	public function Warning(string $message)
+	{
+		$this->WriteEntry(LogLevel::WARNING, $message);		
+	}
+		
+		/**
+	 * Writes a message of the level 'Info'
+	 *
+	 * @param string $message The message to be logged
+	 * 
+	 * @return void
+	 */
+	public function Info(string $message): void
+	{
+		$this->writeEntry(LogLevel::INFO, $message);
+	}
+
+	/**
+	 * Writes a log entry to the file if the log level is equal to or greater than the specified level
+	 *
+	 * @param int    $level   The log level of the message
+	 * @param string $message The message to be logged
+	 *
+	 * @return void
+	 */
+	private function writeEntry(int $level, string $message): void
+	{
+		if ($this->log_level >= $level) {
+			$entry = $this->concatEntry($level, $message);
+			$this->writeToFile($entry);
+		}
+	}
+
+	/**
+	 * Formats a log entry with the specified log level, timestamp, and message
+	 *
+	 * @param int    $level   The log level of the message
+	 * @param string $message The message to be logged
+	 *
+	 * @return string The formatted log entry
+	 */
+	private function ConcatEntry(int $level, string $message): string
+	{
+		$timestamp = date('d.m.Y H:i:s');
+		
+		return LogLevel::ToString($level) . "\t" . $timestamp . "\t" . $message . "\n";
+	}
+
+	/**
+	 * Writes a log entry to the file and optionally echoes it to the screen
+	 *
+	 * @param string $entry The log entry to be written to the file
+	 *
+	 * @return void
+	 */
+
+	private function WriteToFile(string $entry)
+	{
+		file_put_contents($this->log_file, $entry, FILE_APPEND);
+
+		if($this->echo_logs){
+			echo $entry . '<br>';
+		}
+	}
+}
---
+++ tests/core/FileLoggerTest.php
@@ -0,0 +1,176 @@
+<?php declare(strict_types=1);
+
+use CMS\PhpBackup\Core\FileLogger;
+use CMS\PhpBackup\Core\LogLevel;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers CMS\PhpBackup\Core\FileLogger
+ */
+class FileLoggerTest extends TestCase
+{
+    private FileLogger $logger;
+    private string $log_file_path;
+
+    protected function setUp(): void
+    {
+        $this->log_file_path = __DIR__ . "/../work/f.log";
+        // clean the instance of the singleton to make mocking possible
+        $ref = new \ReflectionProperty('CMS\PhpBackup\Core\FileLogger', 'instance');
+        $ref->setAccessible(true);
+        $ref->setValue(null, null);
+
+        $this->logger = FileLogger::getInstance();
+        $this->logger->setLogFile($this->log_file_path);
+        $this->logger->SetLogLevel(LogLevel::INFO);
+    }
+
+    /**
+     * @covers getInstance()
+     */
+    public function testGetInstance()
+    {
+        $instance1 = FileLogger::getInstance();
+        $instance2 = FileLogger::getInstance();
+
+        $this->assertInstanceOf(FileLogger::class, $instance1);
+        $this->assertInstanceOf(FileLogger::class, $instance2);
+        $this->assertEquals($instance1, $instance2);
+    }
+
+    /**
+     * @covers setLogLevel()
+     */
+    public function testSetLogLevel()
+    {
+        $message = 'This is a message';
+        try {
+            $this->logger->setLogLevel(LogLevel::INFO);
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringStartsWith(LogLevel::toString(LogLevel::INFO), $log_file_content);
+            $this->assertStringEndsWith("set log level to INFO\n", $log_file_content);
+
+            $this->logger->Error($message);   
+            $this->logger->Warning($message);   
+            $this->logger->Info($message);
+
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringContainsString(LogLevel::toString(LogLevel::ERROR), $log_file_content);
+            $this->assertStringContainsString(LogLevel::toString(LogLevel::WARNING), $log_file_content);
+            $this->assertStringContainsString(LogLevel::toString(LogLevel::WARNING), $log_file_content);
+            
+            $this->logger->SetLogLevel(LogLevel::WARNING);
+            // unlink after changing the level, because SetLogLevel will also create a Info
+            unlink($this->log_file_path);
+
+            $this->logger->Error($message);   
+            $this->logger->Warning($message);   
+            $this->logger->Info($message);
+            // Assert that the log entry is written to the file
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringContainsString(LogLevel::toString(LogLevel::ERROR), $log_file_content);
+            $this->assertStringContainsString(LogLevel::toString(LogLevel::WARNING), $log_file_content);
+            $this->assertStringNotContainsString(LogLevel::toString(LogLevel::INFO), $log_file_content);
+        } finally {
+            unlink($this->log_file_path);
+        }
+    }
+
+    /**
+     * @covers ActivateEchoLogs()
+     */
+    public function testActiveEchoLogEntry()
+    {
+        $errorMessage = 'This is an error message';
+        try {
+            $this->logger->ActivateEchoLogs();
+            // Capture output to check if it's echoed when echo_logs is activated
+            ob_start();
+            $this->logger->error($errorMessage);
+            $output = ob_get_clean();    
+            // Assert that the log entry is written to the file
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringContainsString($errorMessage, $log_file_content);
+            $this->assertEquals($output,$log_file_content . '<br>');
+        } finally {
+            unlink($this->log_file_path);
+        }
+    }
+    
+    /**
+     * @covers DeactivateEchoLogs()
+     */
+    public function testDisabledEchoLogEntry()
+    {
+        $errorMessage = 'This is an error message';
+
+        $this->logger->DeactivateEchoLogs();
+
+        try {
+            // Capture output to check if it's echoed when echo_logs is activated
+            ob_start();
+            $this->logger->error($errorMessage);
+            $output = ob_get_clean();    
+            // Assert that the log entry is written to the file
+            $this->assertEmpty($output);
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringContainsString($errorMessage, $log_file_content);
+        } finally {
+            unlink($this->log_file_path);
+        }
+    }
+    
+    /**
+     * @covers error()
+     */
+    public function testErrorLogEntry()
+    {
+        $errorMessage = 'This is an error message';
+        try {
+            $this->logger->error($errorMessage);
+
+            // Assert that the log entry is written to the file
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringStartsWith(LogLevel::toString(LogLevel::ERROR), $log_file_content);
+            $this->assertStringEndsWith($errorMessage . "\n", $log_file_content);
+        } finally {
+            unlink($this->log_file_path);
+        }
+    }
+    
+    /**
+     * @covers error()
+     */
+    public function testWarningLogEntry()
+    {
+        $errorMessage = 'This is a warning message';
+        try {
+            $this->logger->warning($errorMessage);
+
+            // Assert that the log entry is written to the file
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringStartsWith(LogLevel::toString(LogLevel::WARNING), $log_file_content);
+            $this->assertStringEndsWith($errorMessage . "\n", $log_file_content);
+        } finally {
+            unlink($this->log_file_path);
+        }
+    }
+    
+    /**
+     * @covers error()
+     */
+    public function testInfoLogEntry()
+    {
+        $errorMessage = 'This is an error message';
+        try {
+            $this->logger->Info($errorMessage);
+
+            // Assert that the log entry is written to the file
+            $log_file_content = file_get_contents($this->log_file_path);
+            $this->assertStringStartsWith(LogLevel::toString(LogLevel::INFO), $log_file_content);
+            $this->assertStringEndsWith($errorMessage . "\n", $log_file_content);
+        } finally {
+            unlink($this->log_file_path);
+        }
+    }
+}
