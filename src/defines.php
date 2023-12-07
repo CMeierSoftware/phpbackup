4665eb1381eaf65365a3d1a0a533ba8af5cd9801
Christoph <info@cmeier-software.com>
2023-12-07 11:21:57 +0800

    add a temp dir and create dirs if not exists

--- src/defines.php
+++ src/defines.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+error_reporting(E_ALL);
 
 if (!defined('ABS_PATH')) {
     define('ABS_PATH', realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR);
@@ -8,6 +9,16 @@ if (!defined('ABS_PATH')) {
 
 if (!defined('CONFIG_DIR')) {
     define('CONFIG_DIR', ABS_PATH . 'config' . DIRECTORY_SEPARATOR);
+}
+if (!file_exists(CONFIG_DIR)) {
+    mkdir(CONFIG_DIR, 0644, true);
+}
+
+if (!defined('TEMP_DIR')) {
+    define('TEMP_DIR', ABS_PATH . 'temp' . DIRECTORY_SEPARATOR);
+}
+if (!file_exists(TEMP_DIR)) {
+    mkdir(TEMP_DIR, 0644, true);
 }
 
 require_once ABS_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
