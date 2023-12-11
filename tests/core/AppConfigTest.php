efb19f0b2bdc5160e5ffdfafd39a74aee2446e33
Christoph <info@cmeier-software.com>
2023-12-11 10:42:27 +0800

    getDatabase and Directory config

--- tests/core/AppConfigTest.php
+++ tests/core/AppConfigTest.php
@@ -13,16 +13,19 @@ use ReflectionClass;
 class AppConfigTest extends TestCase
 {
     private const TEST_CONFIG_FILE = ABS_PATH . 'tests\\fixtures\\config\\test.xml';
+    private const TEST_EMPTY_CONFIG_FILE = ABS_PATH . 'tests\\fixtures\\config\\empty_config.xml';
+    private AppConfig $config;
 
     protected function setUp(): void
     {
+        copy(self::TEST_EMPTY_CONFIG_FILE, CONFIG_DIR . '\\empty_app.xml');
         copy(self::TEST_CONFIG_FILE, CONFIG_DIR . '\\valid_app.xml');
+        $this->config = AppConfig::loadAppConfig('valid_app');
     }
     
     public function testLoadAppConfigSuccess(): void
     {
         $appConfig = AppConfig::loadAppConfig('valid_app');
-
         $this->assertInstanceOf(AppConfig::class, $appConfig);
     }
 
@@ -32,9 +35,32 @@ class AppConfigTest extends TestCase
         $this->assertNull($nonExistentAppConfig);
     }
 
-    public function testSaveConfig(): void
+    public function testDatabaseConfig(): void
     {
-        
+        $expectedDatabaseConfig = [
+            'adapter' => 'pdo_mysql',
+            'host' => 'db.example.com',
+            'username' => 'dbuser',
+            'password' => 'secret',
+            'dbname' => 'dbproduction',
+        ];
+    
+        $actualDatabaseConfig = $this->config->getDatabase();
+    
+        $this->assertIsArray($actualDatabaseConfig);   
+
+        foreach ($expectedDatabaseConfig as $key => $value) {
+            $this->assertArrayHasKey($key, $actualDatabaseConfig);
+            $this->assertEquals($value, $actualDatabaseConfig[$key]);
+        }
     }
+
+    public function testNoDatabaseDefined()
+    {
+        $config = AppConfig::loadAppConfig('empty_app');
+        $actualDatabaseConfig = $config->getDatabase();
+        $this->assertNull($actualDatabaseConfig);
+    }
+    
 }
 
