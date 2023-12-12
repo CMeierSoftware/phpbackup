8e1295197c215b3a0b9ffd473a61b60d6aca0c7d
Christoph <info@cmeier-software.com>
2023-12-12 14:14:39 +0800

    use ext lib for file encryption

--- src/core/FileCrypt.php
+++ src/core/FileCrypt.php
@@ -4,58 +4,16 @@ declare(strict_types=1);
 
 namespace CMS\PhpBackup\Core;
 
-use CMS\PhpBackup\Exceptions\FileNotFoundException;
-
 if (!defined('ABS_PATH')) {
     return;
 }
 
+use CMS\PhpBackup\Exceptions\FileNotFoundException;
+use Defuse\Crypto\File;
+
 abstract class FileCrypt
 {
-    private const METHOD = 'aes-256-cbc';
-    private const CHUNK_SIZE = 4096; // 4KB
-
-    /**
-     * Encrypts a file and deletes the original file.
-     *
-     * @param string $inputFile Path to the input file.
-     * @param string $outputFile Path to the output (encrypted) file.
-     * @param string $key Encryption key.
-     * @throws \Exception If the encryption fails or the original file cannot be deleted.
-     */
-    public static function encryptFile($inputFile, $key)
-    {
-        if (!extension_loaded('openssl')) {
-            throw new \RuntimeException("Module 'openssl' is not loaded.");
-        }
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::METHOD));
-
-        self::readWriteFile($inputFile, function ($chunk) use ($key, $iv) {
-            return openssl_encrypt($chunk, self::METHOD, $key, OPENSSL_RAW_DATA, $iv);
-        });
-    }
-
-    /**
-     * Decrypts an encrypted file and deletes the original encrypted file.
-     *
-     * @param string $inputFile Path to the encrypted file.
-     * @param string $outputFile Path to the output (decrypted) file.
-     * @param string $key Decryption key.
-     * @throws \Exception If the decryption fails or the original encrypted file cannot be deleted.
-     */
-    public static function decryptFile($inputFile, $key)
-    {
-        if (!extension_loaded('openssl')) {
-            throw new \RuntimeException("Module 'openssl' is not loaded.");
-        }
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::METHOD));
-
-        self::readWriteFile($inputFile, function ($chunk) use ($key, $iv) {
-            return openssl_decrypt($chunk, self::METHOD, $key, OPENSSL_RAW_DATA, $iv);
-        });
-    }
-
-    private static function readWriteFile(string $inputFile, callable $fnc)
+    public static function encryptFile(string $inputFile, string $key)
     {
         if (!file_exists($inputFile)) {
             throw new FileNotFoundException();
@@ -64,34 +22,34 @@ abstract class FileCrypt
         $tempFile = $inputFile . uniqid();
 
         try {
-            $outputHandle = fopen($tempFile, 'wb');
-            if ($outputHandle === false) {
-                throw new \Exception('Failed to open the output file for writing.');
-            }
+            File::encryptFileWithPassword($inputFile, $tempFile, $key);
+        } catch (\Exception $th) {
+            unlink($tempFile);
+            throw $th;
+        }
 
-            $inputHandle = fopen($inputFile, 'rb');
-            if ($inputHandle === false) {
-                throw new \Exception('Failed to open the input file for reading.');
-            }
+        // Delete the original file
+        if (!unlink($inputFile)) {
+            throw new \Exception('Failed to delete the original file after encryption.');
+        }
 
-            while (!feof($inputHandle)) {
-                $chunk = fread($inputHandle, self::CHUNK_SIZE);
-                if (false === $chunk) {
-                    throw new \Exception('Could not read file.');
-                }
+        if (!rename($tempFile, $inputFile)) {
+            throw new \Exception('Failed to rename the temporary file after encryption.');
+        }
+    }
 
-                $modifiedChunk = $fnc($chunk);
-                if ($modifiedChunk === false) {
-                    throw new \Exception('Encryption failed.');
-                }
+    public static function decryptFile(string $inputFile, string $key)
+    {
+        if (!file_exists($inputFile)) {
+            throw new FileNotFoundException();
+        }
 
-                if (false === fwrite($outputHandle, $modifiedChunk)) {
-                    throw new \Exception('Could not write file.');
-                }
-            }
-        } finally {
-            fclose($inputHandle);
-            fclose($outputHandle);
+        $tempFile = $inputFile . uniqid();
+        try {
+            File::decryptFileWithPassword($inputFile, $tempFile, $key);
+        } catch (\Exception $th) {
+            unlink($tempFile);
+            throw $th;
         }
 
         // Delete the original file
--- tests/core/FileCryptTest.php
+++ tests/core/FileCryptTest.php
@@ -6,26 +6,31 @@ namespace CMS\PhpBackup\Tests;
 
 use CMS\PhpBackup\Core\FileCrypt;
 use CMS\PhpBackup\Exceptions\FileNotFoundException;
+use CMS\PhpBackup\Helper\FileHelper;
 use PHPUnit\Framework\TestCase;
 
 class FileCryptTest extends TestCase
 {
     private const TEST_KEY = 'your_secret_key';
-    private const TEST_FILE_PLAIN = ABS_PATH . 'tests\\work\\file1.txt';
-    private const TEST_FILE_ENCRYPTED = ABS_PATH . 'tests\\work\\encrypted_file1.txt';
+    private const TEST_WORK_DIR = ABS_PATH . 'tests\\work\\';
+    private const TEST_FILE_PLAIN = self::TEST_WORK_DIR . 'file1.txt';
+    private const TEST_FILE_ENCRYPTED = self::TEST_WORK_DIR . 'encrypted_file1.txt';
 
     protected function setUp(): void
     {
-        // if (file_exists(self::TEST_FILE_PLAIN)) {
-        //     unlink(self::TEST_FILE_PLAIN);
-        // }
-        // copy(ABS_PATH .'tests\\fixtures\\zip\\file1.txt', self::TEST_FILE_PLAIN);
+        FileHelper::makeDir(self::TEST_WORK_DIR);
+        copy(ABS_PATH . 'tests\\fixtures\\zip\\file1.txt', self::TEST_FILE_PLAIN);
+        copy(ABS_PATH . 'tests\\fixtures\\encryption\\encrypted_file1.txt', self::TEST_FILE_ENCRYPTED);
 
-        // if (file_exists(self::TEST_FILE_ENCRYPTED)) {
-        //     unlink(self::TEST_FILE_ENCRYPTED);
-        // }
-        // copy(ABS_PATH .'tests\\fixtures\\encryption\\encrypted_file1.txt', self::TEST_FILE_ENCRYPTED);
+        $this->assertFileExists(self::TEST_FILE_PLAIN);
+        $this->assertFileExists(self::TEST_FILE_ENCRYPTED);
     }
+
+    public function tearDown(): void
+    {
+        FileHelper::deleteDirectory(self::TEST_WORK_DIR);
+    }
+
     /**
      * @covers FileCrypt::encryptFile
      * @covers FileCrypt::decryptFile
@@ -53,8 +58,8 @@ class FileCryptTest extends TestCase
      */
     public function testFileDecryption()
     {
-        FileCrypt::decryptFile(self::TEST_FILE_PLAIN, self::TEST_KEY);
-        $this->assertFileExists(self::TEST_FILE_PLAIN);
+        FileCrypt::decryptFile(self::TEST_FILE_ENCRYPTED, self::TEST_KEY);
+        $this->assertFileExists(self::TEST_FILE_ENCRYPTED);
         $this->assertFileEquals(self::TEST_FILE_PLAIN, self::TEST_FILE_ENCRYPTED);
     }
 }
