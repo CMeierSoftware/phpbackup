7160656fff2677916b2de6e36c391b94b4614bcc
Christoph <info@cmeier-software.com>
2023-12-19 08:29:06 +0800

    add docs

--- src/core/AppConfig.php
+++ src/core/AppConfig.php
@@ -6,7 +6,6 @@ namespace CMS\PhpBackup\Core;
 
 use CMS\PhpBackup\Exceptions\FileNotFoundException;
 use CMS\PhpBackup\Helper\FileHelper;
-use InvalidArgumentException;
 use Laminas\Config\Config;
 use Laminas\Config\Factory as LaminasConfigFactory;
 use Laminas\Config\Reader\Xml as XmlReader;
@@ -22,6 +21,12 @@ class AppConfig
     private readonly string $configFile;
     private readonly string $tempDir;
 
+    /**
+     * AppConfig constructor.
+     *
+     * @param string $configFile The path of the configuration file.
+     * @param string $tempDir The path of the temporary directory.
+     */
     private function __construct(string $configFile, string $tempDir)
     {
         $this->configFile = $configFile;
@@ -29,62 +34,56 @@ class AppConfig
         $this->tempDir = $tempDir;
     }
 
-    public static function loadAppConfig(string $app): ?self
+    /**
+     * Loads the application configuration for a specific app.
+     *
+     * @param string $app The app name.
+     * @return self An instance of the AppConfig class.
+     * @throws FileNotFoundException If the configuration file does not exist.
+     */
+    public static function loadAppConfig(string $app): self
     {
         $config_file = CONFIG_DIR . $app . '.xml';
         $tempDir = CONFIG_DIR . self::TMP_DIR . $app;
 
         if (!file_exists($config_file)) {
-            return null;
+            throw new FileNotFoundException("Configuration file does not exist: {$config_file}");
         }
 
         return new self($config_file, $tempDir);
     }
 
-    public function getBackupDatabase(): ?array
-    {
-        return isset($this->config['backup']['database']) ? $this->config['backup']['database'] : null;
-    }
-
-    public function getBackupDirectory(): ?array
-    {
-        return isset($this->config['backup']['directory']) ? $this->config['backup']['directory'] : null;
-    }
-
-    public function getBackupSettings(): ?array
-    {
-        return isset($this->config['backup']['settings']) ? $this->config['backup']['settings'] : null;
-    }
-
-    public function getRemoteSettings(): ?array
-    {
-        return isset($this->config['remote']) ? $this->config['remote'] : null;
-    }
-
-    public function getTempDir(): string
+    /**
+     * Creates the temporary directory if it does not exist.
+     */
+    private function createTempDir(): void
     {
         if (!file_exists($this->tempDir)) {
             FileHelper::makeDir($this->tempDir);
         }
+    }
 
+    /**
+     * Returns the path of the temporary directory.
+     *
+     * @return string The path of the temporary directory.
+     */
+    public function getTempDir(): string
+    {
+        $this->createTempDir();
         return $this->tempDir . DIRECTORY_SEPARATOR;
     }
 
     /**
-     * Write data to a file in JSON format.
+     * Saves temporary data of a specific type.
      *
-     * @param string $type the name of the file without extension
-     * @param array $data the data to be written to the file
-     *
-     * @return bool true on success, false on failure
+     * @param string $type The type of data.
+     * @param array $data The data to be saved.
      */
     public function saveTempData(string $type, array $data): void
     {
-        if (str_contains($type, DIRECTORY_SEPARATOR) || str_contains($type, '\\')) {
-            throw new InvalidArgumentException('type can not contain backslashes or directory separator characters.');
-        }
-        
-        $filePath = $this->getTempDir() . $type . '.xml';
+        $sanitizedType = str_replace([DIRECTORY_SEPARATOR, '\\', '/'], '_', $type);
+        $filePath = $this->getTempDir() . $sanitizedType . '.xml';
         FileLogger::getInstance()->Info("Will write tempData to '{$filePath}'.");
 
         $config = new Config($data, false);
@@ -95,13 +94,13 @@ class AppConfig
     }
 
     /**
-     * Read data from a file in JSON format.
+     * Reads and returns temporary data of a specific type.
      *
-     * @param string $type the name of the file without extension
-     *
-     * @return null|mixed the decoded data, or null on failure
+     * @param string $type The type of data.
+     * @return array The temporary data.
+     * @throws FileNotFoundException If the data file does not exist.
      */
-    public function readTempData(string $type): mixed
+    public function readTempData(string $type): array
     {
         $filePath = $this->getTempDir() . $type . '.xml';
 
@@ -113,5 +112,45 @@ class AppConfig
         FileLogger::getInstance()->Info("Read tempData from '{$filePath}'.");
 
         return $reader->fromFile($filePath);
+    }
+
+    /**
+     * Returns the backup database settings from the configuration file.
+     *
+     * @return array|null The backup database settings.
+     */
+    public function getBackupDatabase(): ?array
+    {
+        return isset($this->config['backup']['database']) ? $this->config['backup']['database'] : null;
+    }
+
+    /**
+     * Returns the backup directory settings from the configuration file.
+     *
+     * @return array|null The backup directory settings.
+     */
+    public function getBackupDirectory(): ?array
+    {
+        return isset($this->config['backup']['directory']) ? $this->config['backup']['directory'] : null;
+    }
+
+    /**
+     * Returns the backup settings from the configuration file.
+     *
+     * @return array|null The backup settings.
+     */
+    public function getBackupSettings(): ?array
+    {
+        return isset($this->config['backup']['settings']) ? $this->config['backup']['settings'] : null;
+    }
+
+    /**
+     * Returns the remote settings from the configuration file.
+     *
+     * @return array|null The remote settings.
+     */
+    public function getRemoteSettings(): ?array
+    {
+        return isset($this->config['remote']) ? $this->config['remote'] : null;
     }
 }
