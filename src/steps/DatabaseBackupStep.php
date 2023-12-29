ac6db89cbabe6fa314c1e805502340b8a28ec8e2
Christoph <info@cmeier-software.com>
2023-12-29 10:31:31 +0800

    Add step For Database backup

---
+++ src/steps/DatabaseBackupStep.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CMS\PhpBackup\Step;
+use CMS\PhpBackup\Backup\DatabaseBackupCreator;
+use CMS\PhpBackup\Core\FileCrypt;
+
+if (!defined('ABS_PATH')) {
+    return;
+}
+
+final class DatabaseBackupStep extends AbstractStep
+{
+    private readonly array $dbConfig;
+    private readonly string $backupFolder;
+    private readonly string $encryptionKey;
+
+    public function __construct(array $dbConfig, string $backupFolder, string $encryptionKey, int $delay = 0)
+    {
+        parent::__construct($delay);
+        $this->dbConfig = $dbConfig;
+        $this->backupFolder = $backupFolder;
+        $this->encryptionKey = $encryptionKey;
+    }
+
+    protected function _execute(): StepResult
+    {
+        $this->logger->Info("start database dump of ({$this->dbConfig['host']}, {$this->dbConfig['dbname']})");
+        $db = new DatabaseBackupCreator($this->dbConfig['host'], $this->dbConfig['username'], $this->dbConfig['password'], $this->dbConfig['dbname']);
+
+        $result = $db->backupMySql();
+
+        if (!$result) {
+            $this->logger->Warning('Database dump could not be created.');
+        } else {
+            $this->logger->Info("Database dump to '{$result}'");
+        }
+
+        FileCrypt::encryptFile($result, $this->encryptionKey);
+
+        $result = $this->copyToTempDir($result, basename($result));
+
+        return new StepResult($result, false);
+    }
+
+    private function copyToTempDir(string $file, string $newName): string
+    {
+        $newFile = $this->backupFolder . $newName;
+        rename($file, $newFile);
+        $this->logger->Info("Move file from '{$file}' to '{$newFile}'");
+
+        return $newFile;
+    }
+}
---
+++ tests/step/DatabaseBackupStepTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CMS\PhpBackup\Tests;
+
+use CMS\PhpBackup\Backup\DatabaseBackupCreator;
+use CMS\PhpBackup\Core\FileCrypt;
+use CMS\PhpBackup\Core\FileLogger;
+use CMS\PhpBackup\Core\LogLevel;
+use CMS\PhpBackup\Helper\FileHelper;
+use CMS\PhpBackup\Step\StepResult;
+use PHPUnit\Framework\TestCase;
+use CMS\PhpBackup\Step\DatabaseBackupStep;
+use PHPUnit\Framework\MockObject\MockObject;
+
+final class DatabaseBackupStepTest extends TestCase
+{
+    private const TEST_DIR = ABS_PATH . 'tests\\work\\test_directory\\';
+    private const LOG_FILE = self::TEST_DIR . self::class . '.log';
+            
+    private const DB_CONFIG = ['host' => 'localhost', 'username' => 'root', 'password' => '', 'dbname' => 'test'];
+    private $databaseBackupStep;
+
+    protected function setUp(): void
+    {
+        FileLogger::getInstance()->setLogFile(self::LOG_FILE);
+        FileLogger::getInstance()->setLogLevel(LogLevel::INFO);
+        $this->databaseBackupStep = new DatabaseBackupStep(
+            self::DB_CONFIG,
+            self::TEST_DIR,
+            'encryption_key',
+            0
+        );
+
+        // Create a temporary test directory for the tests
+        FileHelper::makeDir(self::TEST_DIR);
+        self::assertFileExists(self::TEST_DIR);
+    }
+
+    protected function tearDown(): void
+    {
+        FileHelper::deleteDirectory(self::TEST_DIR);
+    }
+
+    /**
+     * @covers \CMS\PhpBackup\Step\DatabaseBackupStep::_execute()
+     */
+    public function testExecuteWithSuccessfulBackup(): void
+    {
+        $expected = new StepResult('', false);
+        $actual = $this->databaseBackupStep->execute();
+
+        self::assertInstanceOf(StepResult::class, $actual);
+
+        $dtPattern = '/^backup_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.sql\.gz$/';
+        $this->assertMatchesRegularExpression($dtPattern, basename($actual->returnValue));
+        $this->assertStringStartsWith(self::TEST_DIR, $actual->returnValue);
+        $this->assertSame($expected->repeat, $actual->repeat);
+        self::assertFileExists($actual->returnValue);
+    }
+    // Add more test cases as needed
+}
