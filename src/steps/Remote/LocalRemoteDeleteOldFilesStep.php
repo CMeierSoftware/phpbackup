04671cf1224b06c6f02b8b2c6c90e6c1bc566be5
Christoph <info@cmeier-software.com>
2024-01-10 11:39:07 +0800

    implement remote steps for local

---
+++ src/steps/Remote/LocalRemoteDeleteOldFilesStep.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CMS\PhpBackup\Step\Remote;
+
+use CMS\PhpBackup\Core\AppConfig;
+use CMS\PhpBackup\Remote\Local;
+use CMS\PhpBackup\Step\AbstractRemoteDeleteOldFilesStep;
+
+if (!defined('ABS_PATH')) {
+    return;
+}
+
+final class LocalRemoteDeleteOldFilesStep extends AbstractRemoteDeleteOldFilesStep
+{
+    /**
+     * LocalRemoteDeleteOldFilesStep constructor.
+     *
+     * @param AppConfig $config configuration for this step
+     * @param int $delay delay between this and the previous step
+     */
+    public function __construct(AppConfig $config, int $delay = 0)
+    {
+        $remote = new Local($config->getRemoteSettings()['local']['rootDir']);
+        parent::__construct($remote, $config, $delay);
+    }
+}
