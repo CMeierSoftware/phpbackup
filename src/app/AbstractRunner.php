c42ca722876d4106f9ded721e8c3eda0d90d728d
Christoph <info@cmeier-software.com>
2024-01-12 14:43:09 +0800

    add doc

--- src/app/AbstractRunner.php
+++ src/app/AbstractRunner.php
@@ -13,48 +13,87 @@ use CMS\PhpBackup\Core\FileLogger;
 use CMS\PhpBackup\Core\LogLevel;
 use CMS\PhpBackup\Core\StepManager;
 use CMS\PhpBackup\Core\SystemLocker;
-
+/**
+ * Class AbstractRunner
+ *
+ * Represents an abstract runner for executing backup steps.
+ *
+ * @abstract
+ */
 abstract class AbstractRunner
 {
+    /**
+     * @var FileLogger $logger The logger for recording backup activities.
+     * @readonly
+     */
     protected readonly FileLogger $logger;
+
+    /**
+     * @var AppConfig $config The application configuration.
+     * @readonly
+     */
     protected readonly AppConfig $config;
+
+    /**
+     * @var array $steps An array of steps to be executed during the backup process.
+     */
     protected array $steps = [];
+
+    /**
+     * @var StepManager $stepManager The step manager for coordinating the execution of backup steps.
+     * @readonly
+     */
     protected readonly StepManager $stepManager;
 
     /**
-     * Creates a new BaseRunner instance.
+     * AbstractRunner constructor.
+     *
+     * @param AppConfig $config The application configuration.
      */
     public function __construct(AppConfig $config)
     {
         $this->config = $config;
         $this->logger = FileLogger::GetInstance();
-        $this->logger->setLogFile($this->config->getTempDir() . 'debug.log');
-        $this->logger->setLogLevel(LogLevel::INFO);
-        $this->logger->activateEchoLogs();
+        $this->configureLogger();
         $this->steps = $this->setupSteps();
         $this->stepManager = new StepManager($this->steps, $this->config);
     }
 
+    /**
+     * Destructor to unlock the backup directory when the runner is destroyed.
+     */
     public function __destruct()
     {
         $this->unlockBackupDir();
     }
 
+    /**
+     * Configures the logger with default settings.
+     */
+    protected function configureLogger(): void
+    {
+        $this->logger->setLogFile($this->config->getTempDir() . 'debug.log');
+        $this->logger->setLogLevel(LogLevel::INFO);
+        $this->logger->activateEchoLogs();
+    }
+
+    /**
+     * Runs the backup process.
+     *
+     * @throws \Exception If an error occurs during the backup process.
+     */
     public function run(): void
     {
         try {
             $this->lockBackupDir();
-
             $result = $this->stepManager->executeNextStep();
-
             $this->logger->Info((string) $result);
         } catch (\Exception $e) {
             $this->logger->Error($e->getMessage());
-
             throw $e;
+        } finally {
+            $this->logger->Info('Backup done.');
         }
-
-        $this->logger->Info('Backup done.');
     }
 
     /**
@@ -66,7 +105,7 @@ abstract class AbstractRunner
     }
 
     /**
-     * Unlocks the backup directory with the system locker.
+     * Unlocks the backup directory with the system locker if it is locked.
      */
     public function unlockBackupDir(): void
     {
@@ -75,5 +114,10 @@ abstract class AbstractRunner
         }
     }
 
+    /**
+     * Sets up and returns the array of backup steps to be executed.
+     *
+     * @return array The array of backup steps.
+     */
     abstract protected function setupSteps(): array;
 }
--- src/steps/StepConfig.php
+++ src/steps/StepConfig.php
@@ -9,27 +9,53 @@ use CMS\PhpBackup\Core\AppConfig;
 if (!defined('ABS_PATH')) {
     return;
 }
-
+/**
+ * Class StepConfig
+ *
+ * Represents a configuration for a step in a workflow, including the step class and an optional delay.
+ *
+ * @final
+ */
 final class StepConfig
 {
+    /**
+     * @var int $delay The delay (in seconds) before executing the step.
+     * @readonly
+     */
     public readonly int $delay;
+
     private readonly string $stepClass;
 
+    /**
+     * StepConfig constructor.
+     *
+     * @param string $stepClass The fully qualified class name of the step extending AbstractStep.
+     * @param int $delay (Optional) The delay (in seconds) before executing the step. Defaults to 0.
+     *
+     * @throws \UnexpectedValueException If $stepClass is not a class extending AbstractStep or if $delay is less than 0.
+     */
     public function __construct(string $stepClass, int $delay = 0)
     {
         if (!class_exists($stepClass) || !is_subclass_of($stepClass, AbstractStep::class)) {
-            throw new \UnexpectedValueException("All entries in the array must be classes extended AbstractStep. {$stepClass} is not.");
+            throw new \UnexpectedValueException("All entries in the array must be classes extending AbstractStep. {$stepClass} is not.");
         }
 
         if ($delay < 0) {
-            throw new \UnexpectedValueException('Delay must be integer greater or equal than 0.');
+            throw new \UnexpectedValueException('Delay must be an integer greater than or equal to 0.');
         }
 
         $this->stepClass = $stepClass;
         $this->delay = $delay;
     }
 
-    public function getStepObject(AppConfig $appConfig)
+    /**
+     * Creates and returns an instance of the step class with the provided AppConfig and delay.
+     *
+     * @param AppConfig $appConfig The application configuration.
+     *
+     * @return AbstractStep The instance of the step class.
+     */
+    public function getStepObject(AppConfig $appConfig): AbstractStep
     {
         return new $this->stepClass($appConfig, $this->delay);
     }
