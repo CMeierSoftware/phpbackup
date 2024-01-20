3248ed37b49de51515a344bd0173b940403e6319
Christoph <info@cmeier-software.com>
2024-01-20 17:19:02 +0800

    Create an abstract step to list backups in remote storage

---
+++ src/steps/Remote/AbstractRemoteListBackups.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CMS\PhpBackup\Step\Remote;
+
+use CMS\PhpBackup\Core\AppConfig;
+use CMS\PhpBackup\Remote\AbstractRemoteHandler;
+use CMS\PhpBackup\Step\AbstractStep;
+use CMS\PhpBackup\Step\StepResult;
+
+if (!defined('ABS_PATH')) {
+    return;
+}
+
+abstract class AbstractRemoteListBackups extends AbstractStep
+{
+    private readonly AbstractRemoteHandler $remote;
+
+    /**
+     * SendRemoteStep constructor.
+     *
+     * @param AbstractRemoteHandler $remoteHandler remote handler for file transfer
+     * @param AppConfig $config configuration for this step
+     */
+    public function __construct(AbstractRemoteHandler $remoteHandler, AppConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->remote = $remoteHandler;
+    }
+
+    /**
+     * Executes the remote step to send backup archives to a remote server.
+     *
+     * @return StepResult the result of the step execution
+     */
+    protected function _execute(): StepResult
+    {
+        $this->remote->connect();
+        $result = $this->remote->dirList('.');
+
+        return new StepResult($result, false);
+    }
+
+    protected function getRequiredStepDataKeys(): array
+    {
+        return [];
+    }
+}
---
+++ tests/step/AbstractRemoteListBackupsTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CMS\PhpBackup\Tests\Steps;
+
+use CMS\PhpBackup\Helper\FileHelper;
+use CMS\PhpBackup\Remote\Local;
+use CMS\PhpBackup\Step\Remote\AbstractRemoteListBackups;
+use CMS\PhpBackup\Step\StepResult;
+
+/**
+ * @internal
+ *
+ * @covers \CMS\PhpBackup\Step\DeleteOldFilesRemoteStep
+ */
+final class AbstractRemoteListBackupsTest extends TestCaseWithAppConfig
+{
+    private const WORK_DIR_REMOTE_BASE = self::TEST_DIR . 'Remote' . DIRECTORY_SEPARATOR;
+    private Local $remoteHandler;
+    private array $backupFolder = [];
+
+    protected function setUp(): void
+    {
+        FileHelper::makeDir(self::WORK_DIR_REMOTE_BASE);
+        self::assertDirectoryExists(self::WORK_DIR_REMOTE_BASE);
+
+        for ($i = 5; $i < 0; $i++) {
+            $ts = 'backup_' . (new \DateTime())->modify("-{$i} days")->format('Y-m-d_H-i-s') . DIRECTORY_SEPARATOR;
+            FileHelper::makeDir(self::WORK_DIR_REMOTE_BASE . $ts);
+            self::assertDirectoryExists(self::WORK_DIR_REMOTE_BASE . $ts);
+            $this->backupFolder[] = $ts;
+        }
+
+        $this->remoteHandler = new Local(self::WORK_DIR_REMOTE_BASE);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testStep()
+    {        
+        $this->setUpAppConfig('config_full_valid', []);
+        $mockBuilder = $this->getMockBuilder(AbstractRemoteListBackups::class);
+        $mockBuilder->setConstructorArgs([$this->remoteHandler, $this->config]);
+
+        $sendRemoteStep = $mockBuilder->getMockForAbstractClass();
+
+        $result = $sendRemoteStep->execute();
+        self::assertFalse($result->repeat);
+        self::assertSame($this->backupFolder, $result->returnValue);
+    }
+}
