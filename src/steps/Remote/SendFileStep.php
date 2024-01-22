6f87b5d5f653594758a1825a145a64db44e7a9cd
Christoph <info@cmeier-software.com>
2024-01-22 15:04:26 +0800

    missed add

---
+++ src/steps/Remote/SendFileStep.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CMS\PhpBackup\Step\Remote;
+
+use CMS\PhpBackup\Core\AppConfig;
+use CMS\PhpBackup\Exceptions\FileNotFoundException;
+use CMS\PhpBackup\Exceptions\MaximalAttemptsReachedException;
+use CMS\PhpBackup\Remote\AbstractRemoteHandler;
+use CMS\PhpBackup\Step\AbstractStep;
+use CMS\PhpBackup\Step\StepResult;
+
+if (!defined('ABS_PATH')) {
+    return;
+}
+
+final class SendFileStep extends AbstractStep
+{
+    private const FILE_MAPPING_NAME = 'file_mapping.json';
+    private readonly AbstractRemoteHandler $remote;
+    private readonly string $backupDir;
+    private readonly string $backupDirName;
+    private array $archives;
+    private array $uploadedFiles = [];
+
+    /**
+     * SendRemoteStep constructor.
+     *
+     * @param AbstractRemoteHandler $remoteHandler remote handler for file transfer
+     */
+    public function __construct(AbstractRemoteHandler $remoteHandler, AppConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->remote = $remoteHandler;
+    }
+
+    /**
+     * Executes the remote step to send backup archives to a remote server.
+     *
+     * @return StepResult the result of the step execution
+     */
+    protected function _execute(): StepResult
+    {
+        $this->backupDir = $this->stepData['backupDirectory'];
+        $this->backupDirName = basename($this->backupDir);
+        $this->archives = &$this->stepData['archives'];
+
+        $this->remote->connect();
+        $this->getUploadedFiles();
+        $this->createBaseDirectory();
+
+        $filesToUpload = array_diff(array_keys($this->archives), $this->uploadedFiles);
+        foreach ($filesToUpload as $archiveFileName) {
+            $this->sendArchives($archiveFileName);
+        }
+
+        return new StepResult('', count($this->archives) !== count($this->uploadedFiles));
+    }
+
+    protected function getRequiredStepDataKeys(): array
+    {
+        return ['backupDirectory', 'archives'];
+    }
+
+    /**
+     * Retrieves the list of files already uploaded to the remote server.
+     */
+    private function getUploadedFiles(): void
+    {
+        try {
+            $this->uploadedFiles = $this->remote->dirList($this->backupDirName, true);
+        } catch (FileNotFoundException $th) {
+            $this->uploadedFiles = [];
+
+            return;
+        }
+
+        $filesInFileMapping = array_keys($this->downloadFileMapping());
+        $diff = array_diff($this->uploadedFiles, $filesInFileMapping, [self::FILE_MAPPING_NAME]);
+
+        foreach ($diff as $remoteFile) {
+            $this->remote->fileDelete($this->backupDirName . '/' . $remoteFile);
+        }
+
+        $this->uploadedFiles = array_diff($this->remote->dirList($this->backupDirName, true), [self::FILE_MAPPING_NAME]);
+    }
+
+    /**
+     * Creates the base directory on the remote server if it does not exist.
+     */
+    private function createBaseDirectory(): void
+    {
+        if (!$this->remote->fileExists($this->backupDirName)) {
+            $this->remote->dirCreate($this->backupDirName);
+        }
+    }
+
+    /**
+     * Sends the backup archives to the remote server.
+     */
+    private function sendArchives(string $archiveFileName): void
+    {
+        $this->incrementAttemptsCount();
+
+        if ($this->getAttemptCount() > self::MAX_ATTEMPTS) {
+            throw new MaximalAttemptsReachedException("Maximal attempts to upload '{$archiveFileName}' reached (max. " . (string) self::MAX_ATTEMPTS . 'attempts)');
+        }
+
+        $localPath = $this->backupDir . $archiveFileName;
+        $remotePath = $this->backupDirName . '/' . basename($archiveFileName);
+        $this->remote->fileUpload($localPath, $remotePath);
+        $this->uploadedFiles[] = $archiveFileName;
+        $this->updateFileMapping();
+        $this->resetAttemptsCount();
+    }
+
+    /**
+     * Uploads the file mapping to the remote server.
+     *
+     * @return bool true if the file mapping upload is successful, false otherwise
+     */
+    private function updateFileMapping(): bool
+    {
+        $fileMapping = $this->backupDir . self::FILE_MAPPING_NAME;
+        file_put_contents($fileMapping, json_encode($this->archives, JSON_PRETTY_PRINT));
+
+        $remotePath = $this->backupDirName . '/' . basename($fileMapping);
+        if ($this->remote->fileExists($remotePath)) {
+            $this->remote->fileDelete($remotePath);
+        }
+
+        return $this->remote->fileUpload($fileMapping, $remotePath);
+    }
+
+    private function downloadFileMapping(): array
+    {
+        $fileMapping = $this->backupDir . self::FILE_MAPPING_NAME;
+        $remotePath = $this->backupDirName . '/' . basename($fileMapping);
+
+        if (file_exists($fileMapping)) {
+            unlink($fileMapping);
+        }
+
+        try {
+            $this->remote->fileDownload($fileMapping, $remotePath);
+        } catch (FileNotFoundException) {
+            return [];
+        }
+
+        return json_decode(file_get_contents($fileMapping), true);
+    }
+}
